3167725a8f1406fe798fa28b0d150ec37652f4e6
Brahma Keerthi <brahmakeerthihs008@gmail.com>
2022-02-03 16:37:46 +0530

    Operational part updated

--- app/src/main/java/com/example/converto/MainActivity.java
+++ app/src/main/java/com/example/converto/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.converto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView3);
+        editText = findViewById(R.id.editTextNumberDecimal);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Toast.makeText(MainActivity.this, "Thanks for using", Toast.LENGTH_SHORT).show();
+                String input = editText.getText().toString();
+                int kg = Integer.parseInt(input);
+                double pound = 2.205 * kg;
+                textView.setText(kg + " kg = " + pound + " pound");
+            }
+        });
     }
 }
