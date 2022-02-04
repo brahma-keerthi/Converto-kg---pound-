8520b69194fa7958204298c964f273a4df17cac6
Brahma Keerthi <brahmakeerthihs008@gmail.com>
2022-02-04 20:19:42 +0530

    Input solved

--- app/src/main/java/com/example/converto/MainActivity.java
+++ app/src/main/java/com/example/converto/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.converto;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view){
                 Toast.makeText(MainActivity.this, "Thanks for using", Toast.LENGTH_SHORT).show();
-                String input = editText.getText().toString();
+                String input = null;
+                input = editText.getText().toString();
                 double kg = Double.parseDouble(input);
                 double pound = 2.205 * kg;
                 textView.setText(kg + " kg = " + pound + " pound");
